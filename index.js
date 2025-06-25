84616424d642cd6764166106815a5b4a05fa57e3
kngan6297 <kngan6297@gmail.com>
2025-06-25 19:45:54 +0700

    add new feature

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
+
+mongoose.connect("mongodb://103.90.226.216:27017/ecom").then(() => {
+  console.log("Connected to MongoDB");
+});
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  description: String,
+  image: String,
+});
+
+const ProductModel = mongoose.model("Product", productSchema);
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.status(200).send('Welcome to the API');
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to the API");
+});
+
+app.get("/products", async (req, res) => {
+  try {
+    const products = await ProductModel.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/products", async (req, res) => {
+  try {
+    const { name, price, description, image } = req.body;
+    const newProduct = new ProductModel({ name, price, description, image });
+    await newProduct.save();
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 app.listen(8080, () => {
-    console.log('Server is running on port 8080');
+  console.log("Server is running on port 8080");
 });
-
